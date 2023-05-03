50f30aa370c5d138c3640f0ea3b908a4d496ee4c
Sukhi Singh <44445119+SukhiM@users.noreply.github.com>
2023-05-03 19:17:11 -0400

    Method to connect to elephantSQL server

--- database/utils/createDB.js
+++ database/utils/createDB.js
@@ -28,3 +28,32 @@ const config = {
   }
 
 module.exports = createDB;
+
+/*
+const pgtools = require('pgtools');
+const { connectionString } = require('./configDB');
+
+const config = {
+  connectionString: connectionString
+};
+
+// attempt to create DB
+// if it already exists, this does nothing and just connects to
+// the existing db of that name
+const createDB = async () => {
+  try {
+    await pgtools.createdb(config, dbName);
+    console.log(`Successfully created the database: ${dbName}!`);
+  } catch (err) {
+    if (err.name === 'duplicate_database') {
+      console.log(`Database ${dbName} already exists`);
+      return;
+    } else {
+      console.error('createDB error:', err);
+      process.exit(1);
+    }
+  }
+};
+
+module.exports = createDB;
+*/
